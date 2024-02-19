9fddbcf67ada8e880cb8f6ff80f8f4c0410782e0
handiwoker <handiwoker@gmail.com>
2024-02-19 12:38:37 +0100

    adfdas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var inquiryRouter = require('./routes/inquiry');
 const categoriesRouter = require('./routes/categories');
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter)
+app.use('/inquiry', inquiryRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
